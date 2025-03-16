c8d5060e98d767f16957c2a9462f5429df24a8f4
HyunSeo Jung <hsjung02@postech.ac.kr>
2025-03-16 11:55:42 +0900

    Revert "Hs fsr"

--- esp_code/FSR/conf.py
+++
@@ -1,7 +0,0 @@
-# 블루투스 이름 설정
-BLE_NAME = "MAR9"  # 검색 시 표시되는 이름
-
-# 고유 UUID
-BLE_UUID        = "e4de81ee-e12c-421f-98e7-060e0504f78c"           # 기기 고유 UUID
-WRITE_CHAR_UUID = "d162efba-143b-4535-9f2d-cf481f07428e"    # WRITE 특성 UUID
-
